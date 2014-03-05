ecb734020ee2520a45a2cf3aec7648a59f54af7c
Inigo Lopez de Heredia <inigo@akvo.org>
2014-03-05 16:51:51 +0000

    [#72] Use env variables
    * Check for the env variables the script needs
    * Use FLOW_ prefix
    * Use bash scripting style guidelines from Google: http://google-styleguide.googlecode.com/svn/trunk/shell.xml

--- util/upload-apk/script/flow-releases.sh
+++ util/upload-apk/script/flow-releases.sh
@@ -1,45 +1,48 @@
 #!/bin/bash
+set -e
 
 # Script to automatically build and upload the FLOW apk. It needs to be run 
 # from the /survey folder in the mobile repo holding the FLOW apk code.
-# The script makes use of a jar file uploadS3.jar, which uploads the apk
-# to S3. The S3_ACCESS_KEY and S3_SECRET_KEY need to be replaced by the proper
-# credential values for S3. 
-
+# The script makes use of a jar file deploy.jar, which uploads the apk
+# to S3 nd notifies GAE.
+# To deploy FLOW, the following env variables must be set:
+#
+# FLOW_DEPLOY_JAR=/path/to/deploy.jar
+# FLOW_SERVER_CONFIG=/path/to/akvo-flow-server-config
+# FLOW_S3_ACCESS_KEY=your_S3_access_key
+# FLOW_S3_SECRET_KEY=your_S3_secret_key
+# FLOW_GAE_USERNAME=google_username
+# FLOW_GAE_PASSWORD=google_password
+#
 # The scripts reads the version number of the apk directly from the versionName 
 # property in AndroidManifest.xml
 
-UPLOAD_S3_PATH=/path/to/uploadS3jar
-APK_BUILD_DIR=/path/to/apk/build/directory
-SERVER_CONFIG_PATH=/path/to/akvo-flow-server-config/repo
-
-# Configuration
-S3_ACCESS_KEY=s3_access_key
-S3_SECRET_KEY=s3_secret_key
-GAE_USERNAME=username
-GAE_PASSWORD=password
+[[ -n "${FLOW_DEPLOY_JAR}" ]] || { echo "FLOW_DEPLOY_JAR env var needs to be set"; exit 1; }
+[[ -n "${FLOW_SERVER_CONFIG}" ]] || { echo "FLOW_SERVER_CONFIG env var needs to be set"; exit 1; }
+[[ -n "${FLOW_S3_ACCESS_KEY}" ]] || { echo "FLOW_S3_ACCESS_KEY env var needs to be set"; exit 1; }
+[[ -n "${FLOW_S3_SECRET_KEY}" ]] || { echo "FLOW_S3_SECRET_KEY env var needs to be set"; exit 1; }
+[[ -n "${FLOW_GAE_USERNAME}" ]] || { echo "FLOW_GAE_USERNAME env var needs to be set"; exit 1; }
+[[ -n "${FLOW_GAE_PASSWORD}" ]] || { echo "FLOW_GAE_PASSWORD env var needs to be set"; exit 1; }
 
 VERSION=$(sed -n '/android:versionName="/{;s///;s/".*$//;p;d;}' AndroidManifest.xml | tr -d ' ')
 
-rm -r tmp
-rm -r builds
+rm -rf tmp
+rm -rf builds
 mkdir tmp
 mkdir builds
 
-
-find $SERVER_CONFIG_PATH/ -name 'appengine-web.xml' -exec sed -n 's/\(.*\)<application>\(.*\)<\/application>\(.*\)/\2/p' {} \; | sort > tmp/instances.txt
+find $FLOW_SERVER_CONFIG/ -name 'appengine-web.xml' -exec sed -n 's/\(.*\)<application>\(.*\)<\/application>\(.*\)/\2/p' {} \; | sort > tmp/instances.txt
 for i in $(cat tmp/instances.txt); do 
-    rm -r bin
-    rm -r gen
+    rm -rf bin
+    rm -rf gen
     echo '=================================================='
-    if [ -f $SERVER_CONFIG_PATH/$i/survey.properties ]; then
+    if [[ -f $FLOW_SERVER_CONFIG/$i/survey.properties ]]; then
         echo 'generating apk version' $VERSION 'for instance' $i
-        ant flow-release -Dsurvey.properties=$SERVER_CONFIG_PATH/$i/survey.properties >> tmp/antout.txt
+        ant flow-release -Dsurvey.properties=$FLOW_SERVER_CONFIG/$i/survey.properties >> tmp/antout.txt
         mkdir -p builds/$i/$VERSION
         mv bin/fieldsurvey-*.apk builds/$i/$VERSION/
-        echo 'Deploying APK...'
-        java -jar $UPLOAD_S3_PATH/deploy.jar $S3_ACCESS_KEY $S3_SECRET_KEY $i $APK_BUILD_DIR/$i/$VERSION/fieldsurvey-$VERSION.apk $VERSION $GAE_USERNAME $GAE_PASSWORD
+        java -jar $FLOW_DEPLOY_JAR $FLOW_S3_ACCESS_KEY $FLOW_S3_SECRET_KEY $i builds/$i/$VERSION/fieldsurvey-$VERSION.apk $VERSION $FLOW_GAE_USERNAME $FLOW_GAE_PASSWORD
     else
-        'Cannot find survey.properties file for instance' $i
+        echo 'Cannot find survey.properties file for instance' $i
     fi
 done

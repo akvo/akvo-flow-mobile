80d08fa98ae9586a5545c82f2fc4103649a2b9c0
Inigo Lopez de Heredia <inigo@akvo.org>
2014-04-09 17:20:50 +0100

    [#32] Display updated Record information
    * Ensure record is refreshed after registration survey is responded

--- app/src/main/java/org/akvo/flow/activity/RecordActivity.java
+++ app/src/main/java/org/akvo/flow/activity/RecordActivity.java
@@ -49,7 +49,7 @@ import org.akvo.flow.util.ConstantUtil;
 
 public class RecordActivity extends ActionBarActivity implements SurveyListListener, TabListener {
     public static final String EXTRA_SURVEY_GROUP = "survey_group";
-    public static final String EXTRA_RECORD = "record";
+    public static final String EXTRA_RECORD_ID = "record";
     
     //private static final String TAG = RecordActivity.class.getSimpleName();
     
@@ -90,11 +90,9 @@ public class RecordActivity extends ActionBarActivity implements SurveyListListe
         mDatabase = new SurveyDbAdapter(this);
         
         mSurveyGroup = (SurveyGroup) getIntent().getSerializableExtra(EXTRA_SURVEY_GROUP);
-        mRecord = (SurveyedLocale) getIntent().getSerializableExtra(EXTRA_RECORD);
         setTitle(mSurveyGroup.getName());
         
         setupActionBar();
-        displayRecord();
     }
     
     private void setupActionBar() {
@@ -118,6 +116,10 @@ public class RecordActivity extends ActionBarActivity implements SurveyListListe
         super.onResume();
         mDatabase.open();
         mUser = FlowApp.getApp().getUser();
+        // Record might have changed while answering a registration survey
+        String recordId = getIntent().getStringExtra(EXTRA_RECORD_ID);
+        mRecord = mDatabase.getSurveyedLocale(recordId);
+        displayRecord();
         mAdapter.refresh();
     }
 

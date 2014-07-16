73457796524aef185ff73670267e3c39064c8b66
Inigo Lopez de Heredia <inigo@akvo.org>
2014-07-16 10:57:58 +0100

    [#118] Add repository env var
    * Allow script execution from anywhere in the system

--- util/upload-apk/script/flow-releases.sh
+++ util/upload-apk/script/flow-releases.sh
@@ -17,6 +17,7 @@ set -e
 # The scripts reads the version number of the apk directly from the versionName 
 # property in AndroidManifest.xml
 
+[[ -n "${FLOW_MOBILE}" ]] || { echo "FLOW_MOBILE env var needs to be set"; exit 1; }
 [[ -n "${FLOW_DEPLOY_JAR}" ]] || { echo "FLOW_DEPLOY_JAR env var needs to be set"; exit 1; }
 [[ -n "${FLOW_SERVER_CONFIG}" ]] || { echo "FLOW_SERVER_CONFIG env var needs to be set"; exit 1; }
 [[ -n "${FLOW_S3_ACCESS_KEY}" ]] || { echo "FLOW_S3_ACCESS_KEY env var needs to be set"; exit 1; }
@@ -24,7 +25,10 @@ set -e
 [[ -n "${FLOW_GAE_USERNAME}" ]] || { echo "FLOW_GAE_USERNAME env var needs to be set"; exit 1; }
 [[ -n "${FLOW_GAE_PASSWORD}" ]] || { echo "FLOW_GAE_PASSWORD env var needs to be set"; exit 1; }
 
-VERSION=$(sed -n '/android:versionName="/{;s///;s/".*$//;p;d;}' app/src/main/AndroidManifest.xml | tr -d ' ')
+# Move to the project directory
+cd $FLOW_MOBILE
+
+version=$(sed -n '/android:versionName="/{;s///;s/".*$//;p;d;}' app/src/main/AndroidManifest.xml | tr -d ' ')
 
 rm -rf tmp
 rm -rf builds
@@ -42,14 +46,14 @@ for i in $(cat tmp/instances.txt); do
     rm -rf bin
     echo '=================================================='
     if [[ -f $FLOW_SERVER_CONFIG/$i/survey.properties ]]; then
-        filename=builds/$i/$VERSION/flow-$VERSION.apk
-        echo 'generating apk version' $VERSION 'for instance' $i
+        filename=builds/$i/$version/flow-$version.apk
+        echo 'generating apk version' $version 'for instance' $i
         cp $FLOW_SERVER_CONFIG/$i/survey.properties app/src/main/res/raw/survey.properties
-        gradle clean
-        gradle assembleRelease
-        mkdir -p builds/$i/$VERSION
+        ./gradlew clean
+        ./gradlew assembleRelease
+        mkdir -p builds/$i/$version
         mv app/bin/flow.apk $filename
-        java -jar "$FLOW_DEPLOY_JAR" "$FLOW_S3_ACCESS_KEY" "$FLOW_S3_SECRET_KEY" "$i" "$filename" "$VERSION" "$FLOW_GAE_USERNAME" "$FLOW_GAE_PASSWORD"
+        java -jar "$FLOW_DEPLOY_JAR" "$FLOW_S3_ACCESS_KEY" "$FLOW_S3_SECRET_KEY" "$i" "$filename" "$version" "$FLOW_GAE_USERNAME" "$FLOW_GAE_PASSWORD"
     else
         echo 'Cannot find survey.properties file for instance' $i
     fi

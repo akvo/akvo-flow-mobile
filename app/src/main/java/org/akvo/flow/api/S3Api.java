99f521b2a525e4df37a1149df1543199f47727ec
Inigo Lopez de Heredia <inigo@akvo.org>
2014-10-20 16:55:48 +0100

    [#194] Use en_US instead of en

--- app/src/main/java/org/akvo/flow/api/S3Api.java
+++ app/src/main/java/org/akvo/flow/api/S3Api.java
@@ -141,7 +141,7 @@ public class S3Api {
     }
 
     private String getDate() {
-        final DateFormat df = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss ", Locale.ENGLISH);
+        final DateFormat df = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss ", Locale.US);
         df.setTimeZone(TimeZone.getTimeZone("GMT"));
         return df.format(new Date()) + "GMT";
     }

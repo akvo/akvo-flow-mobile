ea841de1b626de514bf9ce6ae6db74d54a099499
Inigo Lopez de Heredia <inigo@akvo.org>
2014-11-11 11:25:47 +0000

    [#204] Dynamically load spinners

--- app/src/main/java/org/akvo/flow/ui/view/CascadeQuestionView.java
+++ app/src/main/java/org/akvo/flow/ui/view/CascadeQuestionView.java
@@ -18,12 +18,17 @@ package org.akvo.flow.ui.view;
 
 import android.content.Context;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.akvo.flow.R;
 import org.akvo.flow.dao.CascadeDB;
 import org.akvo.flow.domain.Level;
+import org.akvo.flow.domain.Node;
 import org.akvo.flow.domain.Question;
 import org.akvo.flow.domain.QuestionResponse;
 import org.akvo.flow.event.SurveyListener;
@@ -31,13 +36,17 @@ import org.akvo.flow.util.FileUtil;
 import org.akvo.flow.util.FileUtil.FileType;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
-public class CascadeQuestionView extends QuestionView {
-    private String[] mLevels;
+public class CascadeQuestionView extends QuestionView implements AdapterView.OnItemSelectedListener {
+    private static final long DEFAULT_VALUE = -1;
+    private static final int POSITION_NONE = -1;
 
-    private LinearLayout mContent;
+    private String[] mLevels;
+    private LinearLayout mSpinnerContainer;
     private TextView mAnswer;
+    private List<Spinner> mSpinners;
 
     private CascadeDB mDatabase;
 
@@ -49,8 +58,10 @@ public class CascadeQuestionView extends QuestionView {
     private void init() {
         setQuestionView(R.layout.cascade_question_view);
 
-        mContent = (LinearLayout)findViewById(R.id.cascade_content);
+        mSpinnerContainer = (LinearLayout)findViewById(R.id.cascade_content);
         mAnswer = (TextView)findViewById(R.id.answer);
+
+        mSpinners = new ArrayList<Spinner>();
 
         // Load level names
         List<Level> levels = getQuestion().getLevels();
@@ -70,6 +81,78 @@ public class CascadeQuestionView extends QuestionView {
         // TODO: We need to determine whether src contains the URL or just the filename of the resource
         mDatabase = new CascadeDB(getContext(), new File(FileUtil.getFilesDir(FileType.RES), src).getAbsolutePath());
         mDatabase.open();
+        update(POSITION_NONE);
+    }
+
+    private void update(int updatedSpinnerIndex) {
+        final int nextLevel = updatedSpinnerIndex + 1;
+
+        // First, clean up descendant spinners (if any)
+        while (nextLevel < mSpinners.size()) {
+            mSpinners.remove(nextLevel);
+            mSpinnerContainer.removeViewAt(nextLevel);
+        }
+
+        // For the path we've got so far
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i <= updatedSpinnerIndex; i++) {
+            Node node = (Node)mSpinners.get(i).getSelectedItem();
+            if (node.getId() != DEFAULT_VALUE) {
+                builder.append(mSpinners.get(i).getSelectedItem().toString());
+                if (i < updatedSpinnerIndex) {
+                    builder.append(",");
+                }
+            }
+        }
+        mAnswer.setText(builder.toString());
+
+        long parent = 0;
+        if (updatedSpinnerIndex >= 0) {
+            Node node = (Node)mSpinners.get(updatedSpinnerIndex).getSelectedItem();
+            if (node.getId() == DEFAULT_VALUE) {
+                return; // Do not load more levels
+            } else {
+                parent = node.getId();
+            }
+        }
+
+        // Get next level values
+        List<Node> values = mDatabase.getValues(parent);
+
+        if (!values.isEmpty()) {
+            Spinner spinner = new Spinner(getContext());
+            spinner.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,
+                    LayoutParams.WRAP_CONTENT));
+
+            // Insert a fake value with the title
+            String value = mLevels != null && mLevels.length >= nextLevel ? mLevels[nextLevel]
+                    : "Select Level " + (nextLevel);
+            Node node = new Node(DEFAULT_VALUE, value);
+            values.add(0, node);
+
+            ArrayAdapter<Node> adapter = new ArrayAdapter<Node>(getContext(),
+                    android.R.layout.simple_spinner_item, values);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setTag(nextLevel);
+            spinner.setAdapter(adapter);
+            spinner.setOnItemSelectedListener(this);
+
+            mSpinnerContainer.addView(spinner);
+            mSpinners.add(spinner);
+        }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        final int index = (Integer)parent.getTag();
+        update(index);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        final int index = (Integer)parent.getTag();
+        update(index);
     }
 
     @Override

4e407b9c0cc88a72c81a35c78f9465cb60a77428
Inigo Lopez de Heredia <inigo@akvo.org>
2014-12-09 09:39:53 +0000

    [#236] Add missing imports

--- app/src/main/java/org/akvo/flow/activity/PlotActivity.java
+++ app/src/main/java/org/akvo/flow/activity/PlotActivity.java
@@ -26,9 +26,11 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 
 import org.akvo.flow.R;

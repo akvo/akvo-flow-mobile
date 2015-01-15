3259df97710da8fe2f67e8dfb0e537520b1a3568
Inigo Lopez de Heredia <inigo@akvo.org>
2015-01-15 15:06:39 +0000

    [#236] Calculate polygon area
    * Add are property for polygons

--- app/src/main/java/org/akvo/flow/ui/map/PolygonFeature.java
+++ app/src/main/java/org/akvo/flow/ui/map/PolygonFeature.java
@@ -10,6 +10,7 @@ import com.google.maps.android.geometry.Point;
 
 import org.akvo.flow.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PolygonFeature extends Feature {
@@ -68,7 +69,8 @@ public class PolygonFeature extends Feature {
             }
         }
 
-        // Properties
+        // ---- Properties ----
+        // Length
         float length = 0f;
         // Init previous with last point, so we compute the last-first distance as well
         LatLng previous = mPoints.size() > 2 ? mPoints.get(mPoints.size()-1) : null;
@@ -82,6 +84,10 @@ public class PolygonFeature extends Feature {
         }
         String lengthVal = String.format("%.2f", length);
         mProperties.add(new Property("length", lengthVal, "Length", lengthVal + "m"));
+
+        // Area
+        String areaVal = String.format("%.2f", area());
+        mProperties.add(new Property("area", areaVal, "Area", areaVal + "m²"));
     }
 
     @Override
@@ -99,16 +105,37 @@ public class PolygonFeature extends Feature {
         return true;
     }
 
+    private double area() {
+        if (mPoints.size() < 3) {
+            return 0f;
+        }
+
+        // First we compute an equal-are projection of the polygon. We use a sinusoidal
+        // projection for now: http://en.wikipedia.org/wiki/Sinusoidal_projection
+        List<Point> points = new ArrayList<>();
+        for (LatLng location : mPoints) {
+            points.add(project(location));
+        }
+
+        // Now we calculate the area, using regular planar techniques.
+        // http://mathworld.wolfram.com/PolygonArea.html
+        double area = 0.0;
+        Point prev = points.get(points.size()-1);// start form the last point
+        int i = 0;
+        while (i < points.size()) {
+            Point point = points.get(i);
+            area += prev.x * point.y - point.x * prev.y;
+            prev = point;
+            i++;
+        }
+        return Math.abs(area) / 2;
+    }
+
     private Point project(LatLng location) {
         // Sinusoidal projection (equal-area)
         double y = location.latitude * LATITUDE_SIZE;
         double x = location.longitude * LATITUDE_SIZE * Math.cos(Math.toRadians(location.latitude));
         return new Point(x, y);
-    }
-
-    private float area(List<Point> points) {
-        // TODO: calculate area
-        return 0;
     }
 
 }

14311b3300d35fb9b9713104d178e0b6a62b59b0
Inigo Lopez de Heredia <inigo@akvo.org>
2015-11-23 11:43:47 +0100

    [#367] Add unit testing for option responses

---
+++ app/src/androidTest/java/org/akvo/flow/serialization/response/value/OptionValueTest.java
@@ -0,0 +1,80 @@
+package org.akvo.flow.serialization.response.value;
+
+import junit.framework.TestCase;
+
+import org.akvo.flow.domain.Option;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OptionValueTest extends TestCase {
+
+    public void testSingle() {
+        List<Option> options = new ArrayList<>();
+        Option option = new Option();
+        option.setText("United Kingdom");
+        option.setCode("UK");
+        options.add(option);
+
+        String response = OptionValue.serialize(options);
+        List<Option> values = OptionValue.deserialize(response);
+
+        assertEquals(1, values.size());
+        assertEquals("United Kingdom", values.get(0).getText());
+        assertEquals("UK", values.get(0).getCode());
+    }
+
+    public void testSingleOther() {
+        List<Option> options = new ArrayList<>();
+        Option option = new Option();
+        option.setText("United Kingdom");
+        option.setCode("OTHER");
+        option.setIsOther(true);
+        options.add(option);
+
+        String response = OptionValue.serialize(options);
+        List<Option> values = OptionValue.deserialize(response);
+
+        assertEquals(1, values.size());
+        assertEquals("United Kingdom", values.get(0).getText());
+        assertEquals("OTHER", values.get(0).getCode());
+        assertTrue(values.get(0).isOther());
+    }
+
+    public void testSingleNoCodes() {
+        List<Option> options = new ArrayList<>();
+        Option option = new Option();
+        option.setText("United Kingdom");
+        options.add(option);
+
+        String response = OptionValue.serialize(options);
+        List<Option> values = OptionValue.deserialize(response);
+
+        assertEquals(1, values.size());
+        assertEquals("United Kingdom", values.get(0).getText());
+        assertNull(values.get(0).getCode());
+    }
+
+    public void testMultiple() {
+        List<Option> options = new ArrayList<>();
+        Option option = new Option();
+        option.setText("United Kingdom");
+        option.setCode("UK");
+        options.add(option);
+
+        option = new Option();
+        option.setText("Spain");
+        option.setCode("ES");
+        options.add(option);
+
+        String response = OptionValue.serialize(options);
+
+        List<Option> values = OptionValue.deserialize(response);
+
+        assertEquals(2, values.size());
+        assertEquals("United Kingdom", values.get(0).getText());
+        assertEquals("UK", values.get(0).getCode());
+        assertEquals("Spain", values.get(1).getText());
+        assertEquals("ES", values.get(1).getCode());
+    }
+}

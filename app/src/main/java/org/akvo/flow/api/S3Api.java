d613eac6e6eb47711368e91e2a40fa34d157e542
Inigo Lopez de Heredia <inigo@akvo.org>
2016-05-31 09:37:28 +0200

    [#443] Avoid integer overflow

--- app/src/main/java/org/akvo/flow/api/S3Api.java
+++ app/src/main/java/org/akvo/flow/api/S3Api.java
@@ -126,8 +126,10 @@ public class S3Api {
     }
 
     public boolean put(String objectKey, File file, String type, boolean isPublic) throws IOException {
+        // Calculate data size, up to 2 GB
+        final int size = file.length() < Integer.MAX_VALUE ? (int)file.length() : -1;
+
         // Get date and signature
-        final int size = (int)file.length();// long version was added on API level 19
         final byte[] rawMd5 = FileUtil.getMD5Checksum(file);
         final String md5Base64 = Base64.encodeToString(rawMd5, Base64.NO_WRAP);
         final String md5Hex = FileUtil.hexMd5(rawMd5);
@@ -143,7 +145,11 @@ public class S3Api {
         try {
             conn = (HttpURLConnection) url.openConnection();
             conn.setDoOutput(true);
-            conn.setFixedLengthStreamingMode(size);
+            if (size > 0) {
+                conn.setFixedLengthStreamingMode(size);
+            } else {
+                conn.setChunkedStreamingMode(0);
+            }
             conn.setRequestMethod("PUT");
             conn.setRequestProperty("Content-MD5", md5Base64);
             conn.setRequestProperty("Content-Type", type);

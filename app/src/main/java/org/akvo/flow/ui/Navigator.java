e6a2eaa1668c8c8e6057d2cf4e82c3cd259e5083
valeria <valeria@akvo.org>
2016-11-15 16:27:30 +0100

    added copyright

--- app/src/main/java/org/akvo/flow/ui/Navigator.java
+++ app/src/main/java/org/akvo/flow/ui/Navigator.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright (C) 2010-2016 Stichting Akvo (Akvo Foundation)
+ *
+ *  This file is part of Akvo FLOW.
+ *
+ *  Akvo FLOW is free software: you can redistribute it and modify it under the terms of
+ *  the GNU Affero General Public License (AGPL) as published by the Free Software Foundation,
+ *  either version 3 of the License or any later version.
+ *
+ *  Akvo FLOW is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Affero General Public License included below for more details.
+ *
+ *  The full license text can also be seen at <http://www.gnu.org/licenses/agpl.html>.
+ */
 package org.akvo.flow.ui;
 
 import android.content.Context;
@@ -7,6 +22,11 @@ import org.akvo.flow.activity.AppUpdateActivity;
 import org.akvo.flow.domain.apkupdate.ApkData;
 import org.akvo.flow.util.StringUtil;
 
+/**
+ * The purpose of this class is to handle navigation to/from activities in one place
+ *
+ * TODO: progressively move all navigation code here
+ */
 public class Navigator {
 
     public Navigator() {

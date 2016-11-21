d8468a987c8352ef7a42b73147d51e147d24b46d
valeria <valeria@akvo.org>
2016-11-21 10:47:39 +0100

    updated copyright notice

--- app/src/test/java/org/akvo/flow/util/StringUtilTest.java
+++ app/src/test/java/org/akvo/flow/util/StringUtilTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2010-2016 Stichting Akvo (Akvo Foundation)
+ *
+ * This file is part of Akvo FLOW.
+ *
+ * Akvo FLOW is free software: you can redistribute it and modify it under the terms of
+ * the GNU Affero General Public License (AGPL) as published by the Free Software Foundation, either version 3 of the License or any later version.
+ *
+ * Akvo FLOW is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License included below for more details.
+ *
+ * The full license text can also be seen at <http://www.gnu.org/licenses/agpl.html>.
+ *
+ */
+
 package org.akvo.flow.util;
 
 import android.test.suitebuilder.annotation.SmallTest;
@@ -16,21 +31,6 @@ import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 
-/*
- *  Copyright (C) 2010-2016 Stichting Akvo (Akvo Foundation)
- *
- *  This file is part of Akvo FLOW.
- *
- *  Akvo FLOW is free software: you can redistribute it and modify it under the terms of
- *  the GNU Affero General Public License (AGPL) as published by the Free Software Foundation,
- *  either version 3 of the License or any later version.
- *
- *  Akvo FLOW is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU Affero General Public License included below for more details.
- *
- *  The full license text can also be seen at <http://www.gnu.org/licenses/agpl.html>.
- */
 @SmallTest
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(TextUtils.class)

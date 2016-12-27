0ee18223cf6794c32f79b330f63020072387628c
valeria <valeria@akvo.org>
2016-12-27 17:54:13 +0100

    [#544] Removed unused methods

--- app/src/main/java/org/akvo/flow/util/ArrayPreferenceUtil.java
+++ app/src/main/java/org/akvo/flow/util/ArrayPreferenceUtil.java
@@ -16,11 +16,10 @@
 
 package org.akvo.flow.util;
 
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-
 import android.content.Context;
 import android.content.res.Resources;
+
+import java.util.StringTokenizer;
 
 /**
  * utility class for manipulating the preference settings that allow for
@@ -66,7 +65,7 @@ public class ArrayPreferenceUtil {
     public static String formSelectedItemString(String[] values,
             boolean[] selected) {
         boolean isFirst = true;
-        StringBuffer buffer = new StringBuffer();
+        StringBuilder buffer = new StringBuilder();
         for (int i = 0; i < selected.length; i++) {
             if (selected[i]) {
                 if (!isFirst) {
@@ -81,47 +80,4 @@ public class ArrayPreferenceUtil {
         return buffer.toString();
     }
 
-    /**
-     * returns an arrayList of codes that correspond to the array positions with
-     * true values
-     * 
-     * @return
-     */
-    public static String[] getSelectedCodes(Context context,
-            boolean[] selectedItems, int codeResourceId) {
-        ArrayList<String> codes = new ArrayList<String>();
-        Resources res = context.getResources();
-        String[] allCodes = res.getStringArray(codeResourceId);
-        for (int i = 0; i < selectedItems.length; i++) {
-            if (selectedItems[i]) {
-                codes.add(allCodes[i]);
-            }
-        }
-        return codes.toArray(new String[codes.size()]);
-    }
-
-    /**
-     * forms a comma-delimited string of array index values used to persist the
-     * selected items to the db.
-     * 
-     * @param
-     * @return
-     */
-    public static String formPreferenceString(
-            boolean[] selectedItems) {
-        StringBuffer newSelection = new StringBuffer();
-        boolean isFirst = true;
-        for (int i = 0; i < selectedItems.length; i++) {
-            if (selectedItems[i]) {
-                if (!isFirst) {
-                    newSelection.append(",");
-                } else {
-                    isFirst = false;
-                }
-                newSelection.append(i);
-            }
-        }
-        return newSelection.toString();
-    }
-    
 }

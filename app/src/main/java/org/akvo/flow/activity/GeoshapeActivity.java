dcaea3abdecd97af2c9865ce9e8f8eae98218294
valeria <valeria@akvo.org>
2017-01-18 17:23:29 +0100

    Fixed compilation errors

--- app/src/main/java/org/akvo/flow/activity/GeoshapeActivity.java
+++ app/src/main/java/org/akvo/flow/activity/GeoshapeActivity.java
@@ -26,6 +26,7 @@ import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.Menu;
@@ -61,6 +62,8 @@ import org.json.JSONObject;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import timber.log.Timber;
 
 public class GeoshapeActivity extends AppCompatActivity
         implements OnMapLongClickListener, OnMarkerDragListener, OnMarkerClickListener,
@@ -480,7 +483,7 @@ public class GeoshapeActivity extends AppCompatActivity
             mAccuracy.setText(
                 getString(R.string.accuracy) + ": " + new DecimalFormat("#").format(location.getAccuracy()) + "m");
             if (location.getAccuracy() <= ACCURACY_THRESHOLD) {
-                mAccuracy.setTextColor(getResources().getColor(R.color.button_green));
+                mAccuracy.setTextColor(ContextCompat.getColor(this, R.color.button_green));
             } else {
                 mAccuracy.setTextColor(Color.RED);
             }

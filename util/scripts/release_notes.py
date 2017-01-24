d03c323006518c818babe58d1e6c6272617d9cde
valeria <valeria@akvo.org>
2017-01-24 19:36:22 +0100

    [#604] Fixed repo name

--- util/scripts/release_notes.py
+++ util/scripts/release_notes.py
@@ -13,8 +13,7 @@ import argparse
 milestone = '2.2.11'
 label_new = '%22New%20and%20noteworthy%22'
 label_resolved = '%22Resolved%20issues%22'
-repo = 'valllllll2000/test_github_api'
-#repo = 'akvo/akvo-flow-mobile'
+repo = 'akvo/akvo-flow-mobile'
 url_new = 'https://api.github.com/search/issues?q=label:%22Ready%20for%20release%22+label:' + label_new + '+milestone:' + milestone + '+repo:' + repo
 url_resolved = 'https://api.github.com/search/issues?q=label:%22Ready%20for%20release%22+label:' + label_resolved + '+milestone:' + milestone + '+repo:' + repo
 output_filename = 'temp.md'

b0848c4707e0e783afb97ef27c9cc7b6d51105ab
valeria <valeria@akvo.org>
2017-03-22 17:50:10 +0100

    [#470] Displaying caddisfly results in detail

---
+++ app/src/main/java/org/akvo/flow/ui/model/caddisfly/CaddisflyTestResult.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2017 Stichting Akvo (Akvo Foundation)
+ *
+ * This file is part of Akvo Flow.
+ *
+ * Akvo Flow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Akvo Flow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Akvo Flow.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.akvo.flow.ui.model.caddisfly;
+
+public class CaddisflyTestResult {
+
+    private int id;
+    private String name;
+    private String value;
+    private String unit;
+
+    public CaddisflyTestResult(int id, String name, String value, String unit) {
+        this.id = id;
+        this.name = name;
+        this.value = value;
+        this.unit = unit;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String buildResultToDisplay() {
+        String stringBuilder = name +
+                ": " +
+                value +
+                " " +
+                unit;
+        return stringBuilder;
+    }
+}

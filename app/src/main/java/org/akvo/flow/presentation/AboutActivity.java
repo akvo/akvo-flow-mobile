ff76aebf30a9db81951f05e9f8a4dfaf66b6078c
valeria <valeria@akvo.org>
2017-05-12 15:38:23 +0200

    [#595] Started adding about activity (WIP)

---
+++ app/src/main/java/org/akvo/flow/presentation/AboutActivity.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017 Stichting Akvo (Akvo Foundation)
+ *
+ * This file is part of Akvo Flow.
+ *
+ * Akvo Flow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Akvo Flow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Akvo Flow.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.akvo.flow.presentation;
+
+import android.os.Bundle;
+
+import org.akvo.flow.R;
+import org.akvo.flow.activity.BackActivity;
+
+public class AboutActivity extends BackActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        setupToolBar();
+    }
+}

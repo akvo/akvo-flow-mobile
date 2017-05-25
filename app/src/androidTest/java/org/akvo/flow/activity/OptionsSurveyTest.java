61030c90d7bcb5ec5b59af4c36098122788933e9
valeria <valeria@akvo.org>
2017-05-25 16:00:34 +0200

    [#613] Fixed option question test

--- app/src/androidTest/java/org/akvo/flow/activity/OptionsSurveyTest.java
+++ app/src/androidTest/java/org/akvo/flow/activity/OptionsSurveyTest.java
@@ -21,24 +21,24 @@ package org.akvo.flow.activity;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
 import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.filters.MediumTest;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.akvo.flow.R;
 import org.akvo.flow.data.database.SurveyDbAdapter;
 import org.akvo.flow.domain.Survey;
 import org.akvo.flow.testhelper.SurveyInstaller;
 import org.akvo.flow.testhelper.SurveyRequisite;
-import org.akvo.flow.util.FileUtil;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
-
-import java.io.IOException;
-import java.io.InputStream;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -51,63 +51,88 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.akvo.flow.tests.R.raw.optionsurvey;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.not;
 
+@MediumTest
+@RunWith(AndroidJUnit4.class)
 public class OptionsSurveyTest {
+
+    private static Survey survey;
+    private static SurveyInstaller installer;
 
     @Rule
     public ActivityTestRule<SurveyActivity> rule = new ActivityTestRule<>(SurveyActivity.class);
-    private SurveyInstaller installer;
-
-    @Before
-    public void init() {
-        Context context = rule.getActivity();
-        installer = new SurveyInstaller(context, new SurveyDbAdapter(context));
-    }
 
     @BeforeClass
-    public static void setRequisite() {
-        SurveyRequisite.setRequisites(InstrumentationRegistry.getTargetContext());
+    public static void beforeClass() {
+        Context targetContext = InstrumentationRegistry.getTargetContext();
+        SurveyRequisite.setRequisites(targetContext);
+        installer = new SurveyInstaller(targetContext, new SurveyDbAdapter(targetContext));
+        survey = installer.installSurvey(optionsurvey, InstrumentationRegistry.getContext());
+    }
+
+    @Before
+    public void beforeEachTest() {
+        newDataPoint();
+    }
+
+    private void newDataPoint() {
+        openDrawer();
+        selectSurvey();
+        clickAddDataPoint();
+    }
+
+    private void clickAddDataPoint() {
+        onView(withId(R.id.new_datapoint)).perform(click());
+    }
+
+    private void selectSurvey() {
+        onView(withText(survey.getName())).check(matches(isDisplayed())).perform(click());
+    }
+
+    private void openDrawer() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
     }
 
     @After
-    public void clearSurveys() {
-        installer.clearSurveys();
+    public void afterEachTest() {
+        Espresso.pressBack();
     }
 
     @AfterClass
-    public static void tearDown() {
+    public static void afterClass() {
         SurveyRequisite.resetRequisites(InstrumentationRegistry.getTargetContext());
+        installer.clearSurveys();
     }
 
     @Test
-    public void canFillOptionsQuestion() throws IOException, InterruptedException {
-        fillOptionsQuestion(optionsurvey, 0);
+    public void canFillOptionsQuestion() throws Exception {
+        fillOptionsQuestion(0);
+
+        verifySubmitButtonEnabled();
+    }
+
+    private void verifySubmitButtonEnabled() {
         onView(allOf(withClassName(endsWith("Button")), withText(R.string.submitbutton)))
                 .check(matches(isEnabled()));
     }
 
-    private Survey fillOptionsQuestion(int surveyResId, int option) throws IOException {
-        Survey survey = getSurvey(surveyResId);
-
-        openDrawer();
-        onView(withText(survey.getName())).check(matches(isDisplayed())).perform(click());
-        onView(withId(R.id.new_datapoint)).perform(click());
+    private void fillOptionsQuestion(int option) {
         onView(withId(R.id.question_tv)).check(matches(isDisplayed()));
         //Ensure we get the RadioButton View with the ID (each button/option has an ID starting from 0) and click it
         onView(allOf(withClassName(endsWith("RadioButton")), withId(option)))
                 .check(matches(isDisplayed())).perform(click());
         onView(withId(R.id.next_btn)).perform(click());
-
-        return survey;
     }
 
-    private Survey getSurvey(int resId) throws IOException {
-        InputStream input = InstrumentationRegistry.getContext().getResources()
-                .openRawResource(resId);
-        return installer.persistSurvey(FileUtil.readText(input));
+    @Test
+    public void cannotSubmitIfNoOptionSelected() throws Exception {
+        onView(withId(R.id.next_btn)).perform(click());
+        verifySubmitButtonDisabled();
     }
 
-    private void openDrawer() {
-        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+    private void verifySubmitButtonDisabled() {
+        onView(allOf(withClassName(endsWith("Button")), withText(R.string.submitbutton)))
+                .check(matches(not(isEnabled())));
     }
 }

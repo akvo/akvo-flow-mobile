ab46a9155e98baff7ebc46baf6582c4ba448eb9e
valeria <valeria@akvo.org>
2017-06-08 16:48:12 +0200

    [#749] WIP added recyclerview for multi barcodes view

--- app/src/main/java/org/akvo/flow/ui/view/BarcodeQuestionViewMultiple.java
+++ app/src/main/java/org/akvo/flow/ui/view/BarcodeQuestionViewMultiple.java
@@ -21,39 +21,19 @@
 package org.akvo.flow.ui.view;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.support.v7.widget.RecyclerView;
 
 import org.akvo.flow.R;
 import org.akvo.flow.domain.Question;
 import org.akvo.flow.domain.QuestionResponse;
-import org.akvo.flow.event.QuestionInteractionEvent;
 import org.akvo.flow.event.SurveyListener;
 import org.akvo.flow.util.ConstantUtil;
-import org.akvo.flow.util.ViewUtil;
 
-/**
- * Question to handle scanning of a barcode. This question relies on the zxing
- * library being installed on the device.
- *
- * @author Christopher Fagiani
- */
-public class BarcodeQuestionViewMultiple extends QuestionView implements OnClickListener {
+public class BarcodeQuestionViewMultiple extends QuestionView {
 
-    private EditText mInputText;
-    private LinearLayout mInputContainer;
     private boolean mMultiple;
+    private RecyclerView responses;
 
     public BarcodeQuestionViewMultiple(Context context, Question q, SurveyListener surveyListener) {
         super(context, q, surveyListener);
@@ -61,142 +41,24 @@ public class BarcodeQuestionViewMultiple extends QuestionView implements OnClick
     }
 
     private void init() {
-        setQuestionView(R.layout.barcode_question_view);
+        setQuestionView(R.layout.barcode_question_view_multiple);
 
         mMultiple = getQuestion().isAllowMultiple();
 
-        mInputContainer = (LinearLayout) findViewById(R.id.input_ll);
+        responses = (RecyclerView)findViewById(R.id.responses_recycler_view);
 
-        mInputText = (EditText) findViewById(R.id.input_text);
-
-        Button mScanBtn = (Button) findViewById(R.id.scan_btn);
-        mScanBtn.setEnabled(!isReadOnly());
-
-        boolean isQuestionLocked = mQuestion.isLocked();
-
-        if (!isQuestionLocked) {
-            View manualInputContainer = findViewById(R.id.manual_input_container);
-            View manualInputSeparator = findViewById(R.id.manual_input_separator);
-            manualInputSeparator.setVisibility(VISIBLE);
-            manualInputContainer.setVisibility(VISIBLE);
-            setInputText();
-            setUpAddButton();
-        }
-
-        mScanBtn.setOnClickListener(this);
     }
 
-    private void setUpAddButton() {
-        ImageButton mAddBtn = (ImageButton) findViewById(R.id.add_btn);
-        if (isReadOnly() || !mMultiple) {
-            mAddBtn.setVisibility(View.GONE);
-        }
-        mAddBtn.setOnClickListener(this);
-    }
-
-    private void setInputText() {
-        boolean isReadOnly = isReadOnly();
-        if (mMultiple) {
-            mInputText.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                    // EMPTY
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    // EMPTY
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-                    String[] tokens = s.toString().split("\\s+", -1);
-                    if (tokens.length > 1) {
-                        for (int i = 0; i < tokens.length - 1; i++) {
-                            addValue(tokens[i]);
-                        }
-                        mInputText.setText(tokens[tokens.length - 1]);
-                    }
-                }
-            });
-            if (isReadOnly) {
-                mInputText.setVisibility(View.GONE);
-            }
-        }
-        mInputText.setEnabled(!mQuestion.isLocked());
-        mInputText.setFocusable(!isReadOnly);
-    }
-
-    private void addValue(final String text) {
-        LayoutInflater inflater = LayoutInflater.from(getContext());
-        final View view = inflater.inflate(R.layout.barcode_item, mInputContainer, false);
-        ((EditText) view.findViewById(R.id.input)).setText(text);
-        ImageButton btn = (ImageButton) view.findViewById(R.id.delete);
-        if (isReadOnly()) {
-            btn.setVisibility(View.GONE);
-        } else {
-            btn.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    ViewUtil.showConfirmDialog(R.string.deleteresponse, R.string.clear_value_msg,
-                            getContext(), true,
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    mInputContainer.removeView(view);
-                                    displayOrder();
-                                    captureResponse();
-                                }
-                            });
-                }
-            });
-        }
-
-        mInputContainer.addView(view);
-        displayOrder();
-        captureResponse();
-    }
-
-    private void displayOrder() {
-        if (!mQuestion.isAllowMultiple()) {
-            return;
-        }
-        for (int i = 0; i < mInputContainer.getChildCount(); i++) {
-            View view = mInputContainer.getChildAt(i);
-            TextView orderView = (TextView) view.findViewById(R.id.order);
-            String text = i + 1 + ":";
-            orderView.setText(text);
-            orderView.setVisibility(VISIBLE);
-        }
-    }
-
-    /**
-     * handle the action button click
-     */
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.scan_btn:
-                notifyQuestionListeners(QuestionInteractionEvent.SCAN_BARCODE_EVENT);
-                break;
-            case R.id.add_btn:
-                final String value = mInputText.getText().toString();
-                if (!TextUtils.isEmpty(value)) {
-                    addValue(value);
-                    mInputText.setText("");
-                }
-                break;
-        }
-    }
 
     @Override
     public void questionComplete(Bundle barcodeData) {
         if (barcodeData != null) {
             String value = barcodeData.getString(ConstantUtil.BARCODE_CONTENT);
-            if (mMultiple) {
-                addValue(value);
-            } else {
-                mInputText.setText(value);
-            }
+//            if (mMultiple) {
+//                addValue(value);
+//            } else {
+//                mInputText.setText(value);
+//            }
             captureResponse();
         }
     }
@@ -208,19 +70,6 @@ public class BarcodeQuestionViewMultiple extends QuestionView implements OnClick
     @Override
     public void rehydrate(QuestionResponse resp) {
         super.rehydrate(resp);
-        mInputContainer.removeAllViews();
-        mInputText.setText("");
-        String answer = resp != null ? resp.getValue() : null;
-        if (!TextUtils.isEmpty(answer)) {
-            if (mMultiple) {
-                String[] values = answer.split("\\|", -1);
-                for (String value : values) {
-                    addValue(value);
-                }
-            } else {
-                mInputText.setText(answer);
-            }
-        }
     }
 
     /**
@@ -229,8 +78,6 @@ public class BarcodeQuestionViewMultiple extends QuestionView implements OnClick
     @Override
     public void resetQuestion(boolean fireEvent) {
         super.resetQuestion(fireEvent);
-        mInputContainer.removeAllViews();
-        mInputText.setText("");
     }
 
     /**
@@ -239,22 +86,22 @@ public class BarcodeQuestionViewMultiple extends QuestionView implements OnClick
      */
     public void captureResponse(boolean suppressListeners) {
         StringBuilder builder = new StringBuilder();
-        if (mMultiple) {
-            for (int i = 0; i < mInputContainer.getChildCount(); i++) {
-                View v = mInputContainer.getChildAt(i);
-                String value = ((EditText) v.findViewById(R.id.input)).getText().toString();
-                if (!TextUtils.isEmpty(value)) {
-                    builder.append(value);
-                    if (i < mInputContainer.getChildCount() - 1) {
-                        builder.append("|");
-                    }
-                }
-            }
-        }
-        String value = mInputText.getText().toString();
-        if (!TextUtils.isEmpty(value)) {
-            builder.append(value);
-        }
+//        if (mMultiple) {
+//            for (int i = 0; i < mInputContainer.getChildCount(); i++) {
+//                View v = mInputContainer.getChildAt(i);
+//                String value = ((EditText) v.findViewById(R.id.input)).getText().toString();
+//                if (!TextUtils.isEmpty(value)) {
+//                    builder.append(value);
+//                    if (i < mInputContainer.getChildCount() - 1) {
+//                        builder.append("|");
+//                    }
+//                }
+//            }
+//        }
+//        String value = mInputText.getText().toString();
+//        if (!TextUtils.isEmpty(value)) {
+//            builder.append(value);
+//        }
         setResponse(new QuestionResponse(builder.toString(), ConstantUtil.VALUE_RESPONSE_TYPE,
                         getQuestion().getId()),
                 suppressListeners);

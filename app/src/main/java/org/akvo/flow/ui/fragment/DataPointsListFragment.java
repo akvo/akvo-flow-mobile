81da458eb118e2e4de8f3a85c68a1e79b5fa5d7b
valeria <valeria@akvo.org>
2017-07-03 17:07:12 +0200

    [#583] Added default switch

--- app/src/main/java/org/akvo/flow/ui/fragment/DataPointsListFragment.java
+++ app/src/main/java/org/akvo/flow/ui/fragment/DataPointsListFragment.java
@@ -267,9 +267,9 @@ public class DataPointsListFragment extends Fragment implements LocationListener
             case R.id.sync_records:
                 requestRemoteDataRefresh();
                 return true;
+            default:
+                return false;
         }
-
-        return false;
     }
 
     @Override

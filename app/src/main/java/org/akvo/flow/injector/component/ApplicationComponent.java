a1a55dc01a61c981dc4cf3582ec73084da977d64
valeria <valeria@akvo.org>
2017-07-17 16:45:08 +0200

    [#583] Fixed some of the compilation errors

--- app/src/main/java/org/akvo/flow/injector/component/ApplicationComponent.java
+++ app/src/main/java/org/akvo/flow/injector/component/ApplicationComponent.java
@@ -22,10 +22,13 @@ package org.akvo.flow.injector.component;
 
 import android.content.Context;
 
+import com.squareup.sqlbrite.BriteDatabase;
+
 import org.akvo.flow.app.FlowApp;
 import org.akvo.flow.domain.executor.PostExecutionThread;
 import org.akvo.flow.domain.executor.ThreadExecutor;
 import org.akvo.flow.domain.repository.FileRepository;
+import org.akvo.flow.domain.repository.SurveyRepository;
 import org.akvo.flow.domain.repository.UserRepository;
 import org.akvo.flow.injector.module.ApplicationModule;
 import org.akvo.flow.injector.module.ViewModule;
@@ -41,6 +44,10 @@ import dagger.Component;
         ApplicationModule.class, ViewModule.class
 })
 public interface ApplicationComponent {
+
+    SurveyRepository surveyRepository();
+
+    BriteDatabase provideDatabase();
 
     void inject(FlowApp app);
 

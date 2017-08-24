1561849a220d68318148fd81657bc737cb5585a1
valeria <valeria@akvo.org>
2017-08-24 13:29:05 +0200

    [#844] Fixed issue of dispasable becoming unusable after we switch to another survey

--- domain/src/main/java/org/akvo/flow/domain/interactor/UseCase.java
+++ domain/src/main/java/org/akvo/flow/domain/interactor/UseCase.java
@@ -66,7 +66,7 @@ public abstract class UseCase {
 
     public void dispose() {
         if (!disposables.isDisposed()) {
-            disposables.dispose();
+            disposables.clear();
         }
     }
 

42adcba9879bd9bdc1d8b2af8c5e90e1b477467a
valeria <valeria@akvo.org>
2017-10-04 18:17:59 +0200

    [#501] Added data section click actions

--- app/src/main/java/org/akvo/flow/presentation/settings/PreferenceActivity.java
+++ app/src/main/java/org/akvo/flow/presentation/settings/PreferenceActivity.java
@@ -20,24 +20,41 @@
 
 package org.akvo.flow.presentation.settings;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.text.TextUtils;
+import android.text.method.DigitsKeyListener;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.akvo.flow.BuildConfig;
 import org.akvo.flow.R;
 import org.akvo.flow.activity.BackActivity;
+import org.akvo.flow.app.FlowApp;
+import org.akvo.flow.async.ClearDataAsyncTask;
+import org.akvo.flow.data.database.SurveyDbAdapter;
 import org.akvo.flow.injector.component.DaggerViewComponent;
 import org.akvo.flow.injector.component.ViewComponent;
+import org.akvo.flow.service.DataSyncService;
+import org.akvo.flow.service.SurveyDownloadService;
 import org.akvo.flow.ui.Navigator;
+import org.akvo.flow.util.ViewUtil;
 
 import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import timber.log.Timber;
 
-public class PreferenceActivity extends BackActivity {
+public class PreferenceActivity extends BackActivity implements PreferenceView {
 
     @Inject
     Navigator navigator;
@@ -53,6 +70,9 @@ public class PreferenceActivity extends BackActivity {
 
     @BindView(R.id.preference_instance_value)
     TextView instanceNameTv;
+
+    @Inject
+    PreferencePresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -101,5 +121,162 @@ public class PreferenceActivity extends BackActivity {
     private void setUpPreferences() {
         //TODO: retrieve values in preferences
         instanceNameTv.setText(BuildConfig.INSTANCE_URL);
+    }
+
+    @OnClick(R.id.send_data_points)
+    void onDataPointSendTap() {
+        Intent i = new Intent(this, DataSyncService.class);
+        getApplicationContext().startService(i);
+        finish();
+        //TODO: display some confirmation to the user
+    }
+
+    @OnClick({R.id.preference_delete_collected_data_title,
+            R.id.preference_delete_collected_data_subtitle
+    })
+    void onDeleteCollectedDataTap() {
+        //TODO: refactor
+        ViewUtil.showAdminAuthDialog(this, new ViewUtil.AdminAuthDialogListener() {
+            @Override
+            public void onAuthenticated() {
+               deleteData(true);
+            }
+        });
+    }
+
+    @OnClick({R.id.preference_delete_everything_title,
+            R.id.preference_delete_everything_subtitle
+    })
+    void onDeleteAllTap() {
+        //TODO: refactor
+        ViewUtil.showAdminAuthDialog(this, new ViewUtil.AdminAuthDialogListener() {
+            @Override
+            public void onAuthenticated() {
+                deleteData(false);
+            }
+        });
+    }
+
+    @OnClick({R.id.preference_download_form_title,
+            R.id.preference_download_form_subtitle
+    })
+    void onDownloadFormOptionTap() {
+        ViewUtil.showAdminAuthDialog(this, new ViewUtil.AdminAuthDialogListener() {
+
+            @Override
+            public void onAuthenticated() {
+                AlertDialog.Builder inputDialog = new AlertDialog.Builder(PreferenceActivity.this);
+                inputDialog.setTitle(R.string.downloadsurveylabel);
+                inputDialog.setMessage(R.string.downloadsurveyinstr);
+
+                // Set an EditText view to get user input
+                final EditText input = new EditText(PreferenceActivity.this);
+
+                input.setKeyListener(new DigitsKeyListener(false, false));
+                inputDialog.setView(input);
+                inputDialog.setPositiveButton(R.string.okbutton, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String surveyId = input.getText().toString().trim();
+                        if (!TextUtils.isEmpty(surveyId)) {
+                            Intent i = new Intent(PreferenceActivity.this,
+                                    SurveyDownloadService.class);
+                            i.putExtra(SurveyDownloadService.EXTRA_SURVEY_ID, surveyId);
+                            PreferenceActivity.this.startService(i);
+                        }
+                    }
+                });
+
+                inputDialog.setNegativeButton(R.string.cancelbutton, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Canceled.
+                    }
+                });
+
+                inputDialog.show();
+            }
+        });
+    }
+
+    @OnClick({R.id.preference_reload_forms_title,
+            R.id.preference_reload_forms_subtitle
+    })
+    void onReloadAllSurveysOptionTap() {
+        ViewUtil.showAdminAuthDialog(this, new ViewUtil.AdminAuthDialogListener() {
+            @Override
+            public void onAuthenticated() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(PreferenceActivity.this);
+                builder.setTitle(R.string.conftitle);
+                builder.setMessage(R.string.reloadconftext);
+                builder.setPositiveButton(R.string.okbutton, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Context c = PreferenceActivity.this;
+                        Intent i = new Intent(c, SurveyDownloadService.class);
+                        i.putExtra(SurveyDownloadService.EXTRA_DELETE_SURVEYS, true);
+                        c.startService(i);
+                    }
+                });
+                builder.setNegativeButton(R.string.cancelbutton,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                builder.show();
+            }
+        });
+    }
+
+    /**
+     * Permanently deletes data from the device. If unsubmitted data is found on
+     * the database, the user will be prompted with a message to confirm the
+     * operation.
+     *
+     * @param responsesOnly Flag to specify a partial deletion (user generated
+     *                      data).
+     */
+    private void deleteData(final boolean responsesOnly) throws SQLException {
+        try {
+            int messageId;
+            if (unsentData()) {
+                messageId = R.string.unsentdatawarning;
+            } else if (responsesOnly) {
+                messageId = R.string.delete_responses_warning;
+            } else {
+                messageId = R.string.deletealldatawarning;
+            }
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(messageId)
+                    .setCancelable(true)
+                    .setPositiveButton(R.string.okbutton, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            if (!responsesOnly) {
+                                // Delete everything implies logging the current user out (if any)
+                                FlowApp.getApp().setUser(null);
+                            }
+                            new ClearDataAsyncTask(PreferenceActivity.this).execute(responsesOnly);
+                        }
+                    })
+                    .setNegativeButton(R.string.cancelbutton,
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+            builder.show();
+        } catch (SQLException e) {
+            Timber.e(e, e.getMessage());
+            Toast.makeText(this, R.string.clear_data_error, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean unsentData() throws SQLException {
+        SurveyDbAdapter db = new SurveyDbAdapter(this);
+        try {
+            db.open();
+            return db.getUnsyncedTransmissions().size() > 0;
+        } finally {
+            db.close();
+        }
     }
 }

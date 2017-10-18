3d95d54cf259debf2fedee995cb6cc6e3664d926
valeria <valeria@akvo.org>
2017-10-18 09:21:00 +0200

    [#833] Fixes after merge

--- app/src/main/java/org/akvo/flow/ui/Navigator.java
+++ app/src/main/java/org/akvo/flow/ui/Navigator.java
@@ -29,10 +29,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.annotation.StringRes;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import org.akvo.flow.BuildConfig;

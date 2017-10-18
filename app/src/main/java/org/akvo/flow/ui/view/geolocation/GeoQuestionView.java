789c201f22dcf82dcce9e14c8fd3177b41c3a147
valeria <valeria@akvo.org>
2017-10-18 09:22:04 +0200

    [#752] Removed unused imports for codacy

--- app/src/main/java/org/akvo/flow/ui/view/geolocation/GeoQuestionView.java
+++ app/src/main/java/org/akvo/flow/ui/view/geolocation/GeoQuestionView.java
@@ -26,13 +26,11 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
 
 import org.akvo.flow.R;
 import org.akvo.flow.domain.Question;

b6cd8e918b65c3ba40a798638f91f9a480a4855a
valeria <valeria@akvo.org>
2017-11-21 17:08:11 +0100

    [#930] added all data read/write tools

---
+++ app/src/androidTest/java/org/akvo/flow/activity/form/data/TestResponse.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2017 Stichting Akvo (Akvo Foundation)
+ *
+ * This file is part of Akvo Flow.
+ *
+ * Akvo Flow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Akvo Flow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Akvo Flow.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.akvo.flow.activity.form.data;
+
+public class TestResponse {
+
+    private String answerType;
+    private int iteration;
+    private String questionId;
+    private String value;
+
+    public TestResponse(String answerType, int iteration, String questionId, String value) {
+        this.answerType = answerType;
+        this.iteration = iteration;
+        this.questionId = questionId;
+        this.value = value;
+    }
+
+    public String getAnswerType() {
+        return answerType;
+    }
+
+    public void setAnswerType(String answerType) {
+        this.answerType = answerType;
+    }
+
+    public int getIteration() {
+        return iteration;
+    }
+
+    public void setIteration(int iteration) {
+        this.iteration = iteration;
+    }
+
+    public String getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(String questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}

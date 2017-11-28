86c53cb2dfbd25084a10705d2df7ecdacf9d1700
valeria <valeria@akvo.org>
2017-11-28 08:07:44 +0100

    [#789] Removed an exception which was too generic

--- app/src/main/java/org/akvo/flow/util/logging/SentryTree.java
+++ app/src/main/java/org/akvo/flow/util/logging/SentryTree.java
@@ -19,11 +19,9 @@
 
 package org.akvo.flow.util.logging;
 
-import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
-import android.system.ErrnoException;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -50,12 +48,6 @@ class SentryTree extends Timber.Tree {
                     java.net.NoRouteToHostException.class,
                     okhttp3.internal.http2.StreamResetException.class
             });
-
-    public SentryTree() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            IGNORED_EXCEPTIONS.add(ErrnoException.class);
-        }
-    }
 
     @Override
     protected void log(int priority, @Nullable String tag, @Nullable String message,

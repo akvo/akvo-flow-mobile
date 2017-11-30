ab90d10381e5b47a3d6e5a4601f6c2bc386f70df
valeria <valeria@akvo.org>
2017-11-30 13:54:14 +0100

    [#930] Hiding media question button when read only

--- app/src/main/java/org/akvo/flow/ui/view/MediaQuestionView.java
+++ app/src/main/java/org/akvo/flow/ui/view/MediaQuestionView.java
@@ -97,7 +97,7 @@ public class MediaQuestionView extends QuestionView implements OnClickListener,
         }
         mMediaButton.setOnClickListener(this);
         if (isReadOnly()) {
-            mMediaButton.setEnabled(false);
+            mMediaButton.setVisibility(GONE);
         }
 
         mImageView.setOnClickListener(this);

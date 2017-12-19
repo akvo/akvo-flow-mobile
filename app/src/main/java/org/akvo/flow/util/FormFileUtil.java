69a8fe24cc0c68fd2cdefbb9b932d27d7c068980
valeria <valeria@akvo.org>
2017-12-19 10:30:46 +0100

    [#959] Removed unused imports

--- app/src/main/java/org/akvo/flow/util/FormFileUtil.java
+++ app/src/main/java/org/akvo/flow/util/FormFileUtil.java
@@ -24,8 +24,6 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import java.io.File;
-
 import javax.inject.Inject;
 
 public class FormFileUtil extends InternalFileUtil {

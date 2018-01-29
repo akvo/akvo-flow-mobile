b3f1dc28258abf2986d4d5bde901653083f07a18
valeria <valeria@akvo.org>
2018-01-29 10:22:44 +0100

    [#930] Removed unused import

--- app/src/androidTest/java/org/akvo/flow/activity/form/submittedformsview/FormActivityReadOnlyTest.java
+++ app/src/androidTest/java/org/akvo/flow/activity/form/submittedformsview/FormActivityReadOnlyTest.java
@@ -68,7 +68,6 @@ import java.util.Map;
 import java.util.TimeZone;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.contrib.RecyclerViewActions.scrollToPosition;

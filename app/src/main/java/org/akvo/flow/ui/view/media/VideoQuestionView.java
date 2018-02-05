bf2b55ac2ddc2448d8a1d76375c08f8d1b9ebd92
valeria <valeria@akvo.org>
2018-02-05 09:32:26 +0100

    [#1000] Media is not needed in this case

--- app/src/main/java/org/akvo/flow/ui/view/media/VideoQuestionView.java
+++ app/src/main/java/org/akvo/flow/ui/view/media/VideoQuestionView.java
@@ -83,8 +83,8 @@ public class VideoQuestionView extends QuestionView implements MediaSyncTask.Dow
     @BindView(R.id.media_download)
     View mDownloadBtn;
 
-    private Media mMedia;
     private ImageLoader imageLoader;
+    private String filename;
 
     public VideoQuestionView(Context context, Question q, SurveyListener surveyListener) {
         super(context, q, surveyListener);
@@ -101,8 +101,6 @@ public class VideoQuestionView extends QuestionView implements MediaSyncTask.Dow
         if (isReadOnly()) {
             mMediaButton.setVisibility(GONE);
         }
-
-        mMedia = null;
     }
 
     private void initialiseInjector() {
@@ -123,7 +121,6 @@ public class VideoQuestionView extends QuestionView implements MediaSyncTask.Dow
 
     @OnClick(R.id.image)
     void onVideoViewClicked() {
-        String filename = mMedia != null ? mMedia.getFilename() : null;
         if (TextUtils.isEmpty(filename) || !(new File(filename).exists())) {
             showImageLoadError();
             return;
@@ -142,7 +139,7 @@ public class VideoQuestionView extends QuestionView implements MediaSyncTask.Dow
         mProgressBar.setVisibility(VISIBLE);
 
         MediaSyncTask downloadTask = new MediaSyncTask(getContext(),
-                new File(mMedia.getFilename()), this);
+                new File(filename), this);
         downloadTask.execute();
     }
 
@@ -155,8 +152,7 @@ public class VideoQuestionView extends QuestionView implements MediaSyncTask.Dow
         String mediaFilePath =
                 mediaData != null ? mediaData.getString(ConstantUtil.MEDIA_FILE_KEY) : null;
         if (mediaFilePath != null && new File(mediaFilePath).exists()) {
-            mMedia = new Media();
-            mMedia.setFilename(mediaFilePath);
+            filename = mediaFilePath;
 
             captureResponse();
             displayThumbnail();
@@ -178,13 +174,14 @@ public class VideoQuestionView extends QuestionView implements MediaSyncTask.Dow
             return;
         }
 
-        mMedia = MediaValue.deserialize(resp.getValue());
+        Media mMedia = MediaValue.deserialize(resp.getValue());
+        filename = mMedia == null ? null : mMedia.getFilename();
 
         displayThumbnail();
-        String filename = mMedia.getFilename();
         if (TextUtils.isEmpty(filename)) {
             return;
         }
+
         // We now check whether the file is found in the local filesystem, and update the path if it's not
         File file = new File(filename);
         if (!file.exists() && isReadOnly()) {
@@ -192,7 +189,7 @@ public class VideoQuestionView extends QuestionView implements MediaSyncTask.Dow
             // Update response, matching the local path. Note: In the future, media responses should
             // not leak filesystem paths, for these are not guaranteed to be homogeneous in all devices.
             file = new File(FileUtil.getFilesDir(FileUtil.FileType.MEDIA), file.getName());
-            mMedia.setFilename(file.getAbsolutePath());
+            filename = file.getAbsolutePath();
             captureResponse();
         }
     }
@@ -203,7 +200,7 @@ public class VideoQuestionView extends QuestionView implements MediaSyncTask.Dow
     @Override
     public void resetQuestion(boolean fireEvent) {
         super.resetQuestion(fireEvent);
-        mMedia = null;
+        filename = null;
         mImageView.setImageDrawable(null);
         hideDownloadOptions();
     }
@@ -211,15 +208,17 @@ public class VideoQuestionView extends QuestionView implements MediaSyncTask.Dow
     @Override
     public void captureResponse(boolean suppressListeners) {
         QuestionResponse response = null;
-        if (mMedia != null && !TextUtils.isEmpty(mMedia.getFilename())) {
+        if (!TextUtils.isEmpty(filename)) {
             Question question = getQuestion();
-            String value = MediaValue.serialize(mMedia);
+            Media media = new Media();
+            media.setFilename(filename);
+            String value = MediaValue.serialize(media);
             response = new QuestionResponse.QuestionResponseBuilder()
                     .setValue(value)
                     .setType(ConstantUtil.VIDEO_RESPONSE_TYPE)
                     .setQuestionId(question.getQuestionId())
                     .setIteration(question.getIteration())
-                    .setFilename(mMedia.getFilename())
+                    .setFilename(filename)
                     .createQuestionResponse();
         }
         setResponse(response);
@@ -228,7 +227,6 @@ public class VideoQuestionView extends QuestionView implements MediaSyncTask.Dow
     private void displayThumbnail() {
         hideDownloadOptions();
 
-        String filename = mMedia != null ? mMedia.getFilename() : null;
         if (TextUtils.isEmpty(filename)) {
             return;
         }

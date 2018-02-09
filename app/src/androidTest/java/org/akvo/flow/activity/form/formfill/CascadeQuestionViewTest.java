a9ce164382c7e348d4d96cbd4973ff7de0056b5a
valeria <valeria@akvo.org>
2018-02-09 09:09:10 +0100

    [#930] Fixed matcher type

--- app/src/androidTest/java/org/akvo/flow/activity/form/formfill/CascadeQuestionViewTest.java
+++ app/src/androidTest/java/org/akvo/flow/activity/form/formfill/CascadeQuestionViewTest.java
@@ -150,7 +150,7 @@ public class CascadeQuestionViewTest {
         cascadeLevelSpinner.check(matches(withSpinnerText(node.getName())));
     }
 
-    public static Matcher withName(final Matcher nameMatcher) {
+    public static Matcher<Node> withName(final Matcher nameMatcher) {
         return new TypeSafeMatcher<Node>() {
             @Override
             public boolean matchesSafely(Node node) {

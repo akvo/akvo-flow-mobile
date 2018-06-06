f38462e8df2efdde82588cb6d4354cd9b160cd48
valeria <valeria@akvo.org>
2018-06-06 15:58:37 +0200

    [#1125] Fixed a env var name +  added check to not allow call on all instances

--- travis/deploy_release.sh
+++ travis/deploy_release.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -z "$1" ]
+  then
+    echo "At least one instance needs to be provided"
+    exit 1
+fi
+
 [[ -n "${GITHUB_API_KEY}" ]] || { echo "GITHUB_API_KEY env var needs to be set"; exit 1; }
 
 git clone https://valllllll2000:$GITHUB_API_KEY@github.com/akvo/akvo-flow-server-config.git

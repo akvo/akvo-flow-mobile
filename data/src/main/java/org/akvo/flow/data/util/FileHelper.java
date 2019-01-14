032c11fefc1825e87406e496acdbb299a63ce647
valeria <valeria@akvo.org>
2019-01-14 16:00:48 +0100

    [#1267] remove TODO

--- data/src/main/java/org/akvo/flow/data/util/FileHelper.java
+++ data/src/main/java/org/akvo/flow/data/util/FileHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Stichting Akvo (Akvo Foundation)
+ * Copyright (C) 2018-2019 Stichting Akvo (Akvo Foundation)
  *
  * This file is part of Akvo Flow.
  *
@@ -177,7 +177,6 @@ public class FileHelper {
         fout.close();
     }
 
-    //TODO: check errors
     public void extractOnlineArchive(ResponseBody responseBody, File targetFolder) {
         InputStream inputStream = responseBody.byteStream();
         extractZipContent(inputStream, targetFolder);

33b951daa6f37a3abf632b7e0822e55947cad398
valeria <valeria@akvo.org>
2019-05-13 16:25:45 +0200

    [#1341] mappers

---
+++ app/src/main/java/org/akvo/flow/presentation/datapoints/map/offline/OfflineAreaMapper.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2019 Stichting Akvo (Akvo Foundation)
+ *
+ * This file is part of Akvo Flow.
+ *
+ * Akvo Flow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Akvo Flow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Akvo Flow.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.akvo.flow.presentation.datapoints.map.offline;
+
+import com.mapbox.mapboxsdk.geometry.LatLngBounds;
+
+import org.akvo.flow.domain.entity.OfflineArea;
+import org.akvo.flow.domain.entity.OfflineBounds;
+
+import javax.inject.Inject;
+
+import androidx.annotation.NonNull;
+
+public class OfflineAreaMapper {
+
+    private final OfflineBoundsMapper offlineBoundsMapper;
+
+    @Inject
+    public OfflineAreaMapper(OfflineBoundsMapper offlineBoundsMapper) {
+        this.offlineBoundsMapper = offlineBoundsMapper;
+    }
+
+    @NonNull
+    public ViewOfflineArea transform(OfflineArea area) {
+        LatLngBounds latLngBounds = offlineBoundsMapper.transform(area.getBounds());
+        return new ViewOfflineArea(area.getName(), latLngBounds, area.getZoom());
+    }
+
+    @NonNull
+    public OfflineArea transform(ViewOfflineArea area) {
+        OfflineBounds latLngBounds = offlineBoundsMapper.transform(area.getBounds());
+        return new OfflineArea(area.getName(), latLngBounds, area.getZoom());
+    }
+}
---
+++ app/src/main/java/org/akvo/flow/presentation/datapoints/map/offline/ViewOfflineArea.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2019 Stichting Akvo (Akvo Foundation)
+ *
+ * This file is part of Akvo Flow.
+ *
+ * Akvo Flow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Akvo Flow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Akvo Flow.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.akvo.flow.presentation.datapoints.map.offline;
+
+import com.mapbox.mapboxsdk.geometry.LatLngBounds;
+
+public class ViewOfflineArea {
+
+    private final String name;
+    private final LatLngBounds bounds;
+    private final double zoom;
+
+    public ViewOfflineArea(String name, LatLngBounds bounds, double zoom) {
+        this.name = name;
+        this.bounds = bounds;
+        this.zoom = zoom;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LatLngBounds getBounds() {
+        return bounds;
+    }
+
+    public double getZoom() {
+        return zoom;
+    }
+}

315d0c3cb622f054ff8e78793cc19147faa3a378
valeria <valeria@akvo.org>
2019-07-18 16:40:03 +0200

    [#1424] re-add deleted classes as will be needed later

---
+++ app/src/main/java/org/akvo/flow/presentation/datapoints/map/entity/MapDataPoint.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2017-2019 Stichting Akvo (Akvo Foundation)
+ *
+ * This file is part of Akvo Flow.
+ *
+ * Akvo Flow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Akvo Flow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Akvo Flow.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.akvo.flow.presentation.datapoints.map.entity;
+
+public class MapDataPoint {
+
+    private final String id;
+    private final String name;
+    private final double latitude;
+    private final double longitude;
+
+    public MapDataPoint(String id, String name, double latitude, double longitude) {
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+}

c903cac672c8292a3f143bd0357dc346bb90619b
valeria <valeria@akvo.org>
2019-07-29 13:20:34 +0200

    [#1426] split up PR: first classes to view one datapoint

---
+++ domain/src/main/java/org/akvo/flow/domain/interactor/datapoints/GetDataPoint.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2019 Stichting Akvo (Akvo Foundation)
+ *
+ * This file is part of Akvo Flow.
+ *
+ * Akvo Flow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Akvo Flow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Akvo Flow.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.akvo.flow.domain.interactor.datapoints;
+
+import org.akvo.flow.domain.executor.PostExecutionThread;
+import org.akvo.flow.domain.executor.ThreadExecutor;
+import org.akvo.flow.domain.repository.SurveyRepository;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import io.reactivex.Single;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class GetDataPoint {
+
+    public static final String PARAM_DATA_POINT_ID = "data_point_id";
+
+    private final SurveyRepository surveyRepository;
+    private final ThreadExecutor threadExecutor;
+    private final PostExecutionThread postExecutionThread;
+    private final CompositeDisposable disposables;
+
+    @Inject
+    public GetDataPoint(SurveyRepository surveyRepository,
+            ThreadExecutor threadExecutor,
+            PostExecutionThread postExecutionThread) {
+        this.surveyRepository = surveyRepository;
+        this.threadExecutor = threadExecutor;
+        this.postExecutionThread = postExecutionThread;
+        this.disposables = new CompositeDisposable();
+    }
+
+    protected <T> Single buildUseCaseObservable(Map<String, T> parameters) {
+        if (parameters == null || !parameters.containsKey(PARAM_DATA_POINT_ID)) {
+            return Single.error(new IllegalArgumentException("Missing dataPointId"));
+        }
+        String dataPointId = (String) parameters.get(PARAM_DATA_POINT_ID);
+        return surveyRepository.getDataPoint(dataPointId);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> void execute(DisposableSingleObserver<T> observer, Map<String, Object> parameters) {
+        final Single<T> observable = buildUseCaseObservable(parameters)
+                .subscribeOn(Schedulers.from(threadExecutor))
+                .observeOn(postExecutionThread.getScheduler());
+        addDisposable(observable.subscribeWith(observer));
+    }
+
+    public void dispose() {
+        if (!disposables.isDisposed()) {
+            disposables.clear();
+        }
+    }
+
+    private void addDisposable(Disposable disposable) {
+        disposables.add(disposable);
+    }
+}

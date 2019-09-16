76ac06f09a2609e293e0b160877644c4a53d1bb5
valeria <valeria@akvo.org>
2019-09-16 12:46:05 +0200

    [#1447] fix compile errors + add mapper

--- app/src/main/java/org/akvo/flow/injector/component/ViewComponent.java
+++ app/src/main/java/org/akvo/flow/injector/component/ViewComponent.java
@@ -35,6 +35,8 @@ import org.akvo.flow.presentation.datapoints.list.DataPointsListFragment;
 import org.akvo.flow.presentation.datapoints.map.DataPointsMapFragment;
 import org.akvo.flow.presentation.datapoints.map.one.DataPointMapActivity;
 import org.akvo.flow.presentation.form.mobiledata.MobileDataSettingDialog;
+import org.akvo.flow.presentation.geoshape.properties.PropertiesDialog;
+import org.akvo.flow.presentation.geoshape.ViewGeoShapeActivity;
 import org.akvo.flow.presentation.help.HelpActivity;
 import org.akvo.flow.presentation.legal.LegalNoticesActivity;
 import org.akvo.flow.presentation.main.MainActivity;
@@ -115,4 +117,8 @@ public interface ViewComponent {
     void inject(MobileDataSettingDialog mobileDataSettingDialog);
 
     void inject(DataPointMapActivity dataPointMapActivity);
+
+    void inject(ViewGeoShapeActivity viewGeoShapeActivity);
+
+    void inject(PropertiesDialog propertiesDialog);
 }

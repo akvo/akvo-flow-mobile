4ed1b082ed8430960f24689fbfb1167d3b72bf4a
valeria <valerie.cardoso@gmail.com>
2020-09-08 09:59:16 +0200

    small fix to release script

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ filename="$HOME/akvo-flow-mobile-deployment-${versionName}-${formattedDate}.log"
 echo "logs will be saved to: ${filename}"
 
 if [[ "${SLACK_WEBHOOK_URL}" ]]; then
-  curl -X POST -H 'Content-type: application/json' --data '{"text":"Will start app release '$versionName'!"}' https://hooks.slack.com/services/${SLACK_WEBHOOK_URL}
+  curl -X POST -H 'Content-type: application/json' --data '{"text":"Will start release '$versionName'!"}' https://hooks.slack.com/services/${SLACK_WEBHOOK_URL}
 else
   echo "Setup SLACK_WEBHOOK_URL to send messages to slack"
 fi
@@ -18,7 +18,7 @@ a=$(grep -c 'generating apk version' ${filename})
 b=$(grep -c 'New APK version successfully stored' ${filename})
 c=$(cat tmp/instances.txt | grep -v ^$ | wc -l)
 
-if [[ "${a}" == "${b}"]] && [ "${a}" == "${c}"]]; then
+if [[ "${a}" == "${b}" ]] && [[ "${a}" == "${c}" ]]; then
   echo "Release successful"
 else
   echo "Some instances failed"
